dc56f3bd75a0fe17176be4daba3e209cdcd4b698
vineelreddy . v <149480070+vineelreddy1@users.noreply.github.com>
2026-02-05 12:20:08 +0530

    practice questions unit 3 to 4

--- Practice/practicequestion/pra2.cpp
+++ Practice/practicequestion/pra2.cpp
@@ -56,20 +56,88 @@ class Calculator{
 
 
 class Bro{
-    int x,y;
+    int x;
 
 
     public:
+    void setX(int a){
+        x = a;
+    }
+    int getx(){
+        return x;
+    }
 
+};
 
-    int setX(int x, int y){
-        cout << x << y;
-        return 0;
+class Book{
+
+    string title;
+    string author;
+
+    static int totalobjectCount;
+    public:
+    Book(string title, string author){
+        this -> title = title;
+        this->author = author;
+        totalobjectCount++;
+    }
+
+    void print(){
+        cout<<"TOtal object count is : " << totalobjectCount;
+    }
+
+    ~Book(){
+        cout << "Object is Deleted";
+        totalobjectCount--;
     }
 
 
-
 };
+
+int Book::totalobjectCount = 0;
+
+
+//UNit 3 Question 5;
+
+//Create a Bottle class with a destructor that prints "Bottle destroyed". Demonstrate destructor calls using function scope.
+
+
+class Bottle{
+    public:
+    Bottle(){
+        cout << "COnstructor Called " << endl;
+    }
+    ~Bottle(){
+        cout << "Destructor called" << endl;
+    }
+};
+void somedunction(){
+    Bottle bottle;
+}
+
+//UNit 3 Question 6;
+ //Use the this pointer in a class constructor to resolve naming conflicts between member variables and parameters.
+
+
+ class Student{
+    string name;
+    int rollno;
+
+    public:
+    Student(string name , int rollno){
+        this->name = name;
+        this -> rollno = rollno;
+    }
+
+    void print(){
+        cout << "Name is : " << name << endl;
+        cout << "Rollno  is : " << rollno << endl;
+
+    }
+ };
+
+
+
 
 
 
@@ -97,9 +165,41 @@ int main(){
     // UNit 3 Question 3
 // Create a class with private data members. Use setter functions that validate input before assigning values. 
 
-Bro bro;
+// Bro bro;
+// bro.setX(1);
+// cout << bro.getx();
 
-bro.setX(1,2);
+
+// UNit 3 Question 4 & //UNit 3 Question 7;
+// Create a Book class with a constructor that initializes title and author. Use a static variable to count how many Book objects are created.
+//7. Create a class with a static variable that tracks how many objects currently exist (increment in constructor, decrement in destructor). 
+
+// Book B1("book1","VIneel");
+// Book b2("book2","Sushanth");
+
+// b2.print();
+
+
+
+//UNit 3 Question 5;
+
+//Create a Bottle class with a destructor that prints "Bottle destroyed". Demonstrate destructor calls using function scope.
+// somedunction();
+// cout << "Back to main";
+
+
+//UNit 3 Question 6;
+ //Use the this pointer in a class constructor to resolve naming conflicts between member variables and parameters. 
+
+// Student s1("VIneel",7);
+// s1.print();
+
+//UNit 3 Question 8;
+
+
+
+
+
 
 
 
